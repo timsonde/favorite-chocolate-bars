1c9cb1c432459dafb3866398a6c3ccc311c67793
Tim Neunherz <tim.neunherz@outlook.de>
2025-01-20 05:50:34 +0100

    feat(settings): Added setting for logging output

---
+++ settings.lua
@@ -0,0 +1,11 @@
+data:extend(
+    {
+        {
+            name = "favorite-chocolate-bars-show-log",
+            setting_type = "runtime-per-user",
+            type = "bool-setting",
+            default_value = false,
+            per_user = true,
+        }
+    }
+)
